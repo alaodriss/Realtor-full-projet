57f7d1ce80541be50cf4a7e466cf3962e4465dd2
alaodriss <alaodriss@gmail.com>
2023-03-21 14:56:22 +0100

    create header components

--- src/App.js
+++ src/App.js
@@ -5,12 +5,13 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import ForgotPassword from "./pages/ForgotPassword.jsx";
 import Offers from "./pages/Offers";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        {/* <ToastContainer position='top-center'/> */}
+      <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/profile" element={<Profile />} />
