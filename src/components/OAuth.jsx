46915a60ec4d2ec4897823006a5d57f7cba6ae22
alaodriss <alaodriss@gmail.com>
2023-03-23 11:21:33 +0100

    complete the oauth functionality

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,9 +1,48 @@
 import React from "react";
 import { FcGoogle } from "react-icons/fc";
+import { toast } from "react-toastify";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import {db} from '../firebase'
+import { doc, getDoc, serverTimestamp, setDoc } from "firebase/firestore";
+import { useNavigate } from "react-router-dom";
+
 
 export default function OAuth() {
+
+  const navigate = useNavigate();
+
+  
+  async function onGoogleClick(){
+    try{
+      const auth = getAuth()
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider)
+      const user = result.user
+
+      //check fofr the user
+  
+      const docRef = doc(db, "users", user.uid);
+      const docSnap = await getDoc(docRef);
+
+      if (!docSnap.exists()) {
+        await setDoc(docRef, {
+          name: user.displayName,
+          email: user.email,
+          timestamp: serverTimestamp(),
+        });
+      }
+
+      navigate("/");
+    } catch (error) {
+      toast.error("Could not authorize with Google");
+
+    }
+  }
+
+
   return (
-    <button className="flex items-center justify-center w-full bg-red-700 text-white px-7 py-3 uppercase text-sm font-medium hover:bg-red-800 active:bg-red-900 shadow-md hover:shadow-lg active:shadow-lg transition duration-150 ease-in-out rounded">
+    <button typeof="button" onClick={
+      onGoogleClick} className="flex items-center justify-center w-full bg-red-700 text-white px-7 py-3 uppercase text-sm font-medium hover:bg-red-800 active:bg-red-900 shadow-md hover:shadow-lg active:shadow-lg transition duration-150 ease-in-out rounded">
       <FcGoogle className="text-2xl bg-white rounded-full mr-2" />
       Continue with Google
     </button>
